e6b7cbc369fd5347774cc64c0c6a3a3cbdb9c04a
K-dongil <kdi1569@gmail.com>
2022-03-22 15:07:44 +0900

    f/feat: create BarChart

---
+++ front/ramen/components/BarCustom.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Chart } from "chart.js";
+import { useRef } from "react";
+import { DataProps } from "./Types";
+
+const BarCustom = ({testData}: {testData:DataProps}) => {
+  const chartRef = useRef<Chart | null>(null);
+
+  // callback creates the chart on the canvas element
+  const canvasCallback = (canvas: HTMLCanvasElement | null) => {
+    if (!canvas) return;else{ chartRef.current?.destroy()}
+
+    const ctx = canvas.getContext("2d");
+    if (ctx) {
+      chartRef.current = new Chart(ctx, {
+        type: "bar",
+        data: {
+          labels: ["One", "Recommend", "Average"],
+          datasets: [
+            {
+              label: "라면 성분",
+              data: [Number(testData.testData[0]), testData.testData[1], testData.testData[2]],
+              backgroundColor: ["rgba(46, 204, 113, 0.2)", "rgba(46, 204, 54, 0.2)", "rgba(46, 204, 70, 0.2)"],
+              borderColor: "black",
+              borderWidth: 1
+            },
+            // {
+            //   label: "권장 섭취량",
+            //   data: [3, 5, 5, 5, 2, 330],
+            //   backgroundColor: "rgba(41, 128, 185, 0.2)", // 남자일 때 : rgba(41, 128, 185, 0.2), 여자일 때 : rgba(192, 57, 43, 0.2)
+            //   borderColor: "black",
+            //   borderWidth: 1
+            // }
+          ]
+        },
+
+        options: { responsive: true}
+      });
+    }
+  };
+
+  return (
+    <div className="self-center w-1/2">
+      <div className="overflow-hidden">
+        <canvas ref={canvasCallback}></canvas>
+      </div>
+    </div>
+  );
+};
+
+export default BarCustom;
