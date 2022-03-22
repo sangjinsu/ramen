29db3dba3e4bf87d600f40a1c7c4f6440872d8d9
K-dongil <kdi1569@gmail.com>
2022-03-22 15:05:36 +0900

    f/feat: props로 넘겨받은 데이터로 chart표현

--- front/ramen/components/PieCustom.tsx
+++ front/ramen/components/PieCustom.tsx
@@ -1,8 +1,9 @@
 import 'chart.js/auto';
 import React from "react";
 import { Pie } from "react-chartjs-2";
+import { DataProps } from './Types';
 
-const PieCustom = () => {
+const PieCustom = ({testData}: {testData:DataProps}) => {
   const options = {
     responsive: true,
     maintainAspectRatio: false,
@@ -12,10 +13,10 @@ const PieCustom = () => {
   };
 
   const pieData = {
-    labels: ["Test1", "Test2"],
+    labels: ["1회제공량", "탄수화물", "단백질", "지방", "당류", "나트륨"],
     datasets: [
       {
-        data: [100, 50],
+        data: testData.testData,
         backgroundColor: ["rgb(232, 189, 125)", "rgb(125, 168, 232)"]
       }
     ]
