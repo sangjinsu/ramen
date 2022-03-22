675263d3c15464fcd67bed9891917b88143c20e1
K-dongil <kdi1569@gmail.com>
2022-03-22 15:30:11 +0900

    f/rename: /detail -> /ramen/detail 변경

---
+++ front/ramen/pages/ramen/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Image from 'next/image';
+import * as React from 'react';
+
+
+const RamentList: NextPage = () => {
+  
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default RamentList
