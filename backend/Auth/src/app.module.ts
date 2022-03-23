be7b63cc4de63d38f378bdb8f35c54bc9d957dce
sangjinsu <climbplant39@kakao.com>
2022-03-23 15:47:03 +0900

    b/fix: db connection

--- backend/Auth/src/app.module.ts
+++ backend/Auth/src/app.module.ts
@@ -1,19 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { getConnectionOptions } from 'typeorm';
 import { MembersModule } from './members/members.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRootAsync({
-      useFactory: async () =>
-        Object.assign(await getConnectionOptions(), {
-          autoLoadEntities: true,
-        }),
-    }),
-    MembersModule,
-    AuthModule,
-  ],
+  imports: [TypeOrmModule.forRoot(), MembersModule, AuthModule],
 })
 export class AppModule {}
