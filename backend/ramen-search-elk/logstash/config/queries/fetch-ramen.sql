5b1a58e10463a4b6a3a795d24aef253a99085b89
suhan <suhan9603@naver.com>
2022-03-25 17:25:56 +0900

    b/feat:update search elk & create admin

--- backend/ramen-search-elk/logstash/config/queries/fetch-ramen.sql
+++ backend/ramen-search-elk/logstash/config/queries/fetch-ramen.sql
@@ -1,6 +1,8 @@
-SELECT *
+SELECT r.ramen_id, r.name, r.english_name, r.brand, r.english_brand, r.noodle, a.crawling_cnt
 FROM dbmaster.ramen r
-JOIN dbmaster.nutrient n ON n.nutrient_id = r.ramen_id
-JOIN dbmaster.composition p ON p.composition_id = r.ramen_id
-JOIN dbmaster.analysis a ON a.analysis_id = r.ramen_id
+join dbmaster.analysis a
+on r.ramen_id = a.analysis_id
+
+
+
 
