070acd3b4a09ada10d3b22e9873383c3495c492d
김지원 <flykimjiwun@naver.com>
2022-03-25 16:24:37 +0900

    fix:Navbar

--- front/ramen/components/NavBar.js
+++ front/ramen/components/NavBar.js
@@ -6,10 +6,7 @@ import { faUser } from '@fortawesome/free-solid-svg-icons'
 
 
 export default function NavBar() {
-  const router = useRouter();
-
-
-  
+  const router = useRouter();  
   return (
     <>
     <Navbar collapseOnSelect expand="lg" bg="bg-white" variant="light">
@@ -19,59 +16,19 @@ export default function NavBar() {
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="me-auto">
       {/* 최종 네브바, 반응형 위함 Link안에서 링크는 없애주고 안에넣기 */}
-
       <Link href="/">
           <a className='navmenu'>카테고리검색 &nbsp;</a>
         </Link>
     <Link href="/index2">
           <a className='navmenu'>키워드검색 &nbsp;</a>
         </Link>
-    {/* <Link href="/SearchResult">
-          <a className='navmenu'>카테고리결과 &nbsp;</a>
-        </Link>
-        <Link href="/SearchKeywordResult">
-          <a className='navmenu'>키워드결과 &nbsp;</a>
-        </Link> */}
+
     <Link href="/SearchTextResult">
           <a className='navmenu'>텍스트결과 &nbsp;</a>
         </Link>
         <Link href="/test">
           <a className='navmenu'>AxiosTest &nbsp;</a>
-        </Link>
-
-    {/* <Nav.Link >
-    <Link href="/">
-    <div><a className='navmenu'>테스트메뉴</a></div>
-
-        </Link>
-    </Nav.Link> */}
-    
-
-
-{/* div감싸주는것만으로도 잘목는다 띄어쓰기나 블럭 속성 */}
-{/* <div>
-    
-        <Link href="/index2">
-          <a className='navmenu'>카테고리검색</a>
-        </Link>
-    </div> */}
-
-    
-        
-      {/* <Nav.Link href="/"><h4>라면검색</h4></Nav.Link>
-      
-      <Nav.Link href="/index2"><h4>전체라면보기</h4></Nav.Link> */}
-    {/* <Link href="/">
-      Link Home
-        </Link>
-        <Link href="/index2">
-          Link index2
-        </Link>
-
-      <Nav.Link href="/"><h4>라면검색</h4></Nav.Link>
-      
-      <Nav.Link href="/index2"><h4>전체라면보기</h4></Nav.Link> */}
-      
+        </Link>      
     </Nav>
     <Nav>
     <div className="search-container">
@@ -93,14 +50,6 @@ export default function NavBar() {
     <Dropdown.Item href="#/action-3">마이페이지</Dropdown.Item>
   </Dropdown.Menu>
 </Dropdown>
-
-    {/* <NavDropdown title="회원메뉴" id="collasible-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">로그인</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">로그아웃</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">회원가입</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">마이페이지</NavDropdown.Item>
-      </NavDropdown> */}
     </Nav>
   </Navbar.Collapse>
   </Container>
