5e3be78c9ab020fd0c36dc3500a90b714192cc87
김지원 <flykimjiwun@naver.com>
2022-03-28 16:50:30 +0900

    f/feat:Footer

--- front/ramen/components/Footer.js
+++ front/ramen/components/Footer.js
@@ -5,19 +5,31 @@ export default function NavBar() {
   
   return (
     <>
-    <hr></hr>
+    
     <Container>
       <Row>
-        <Col></Col>
-        <Col>
-        <h1>Footer</h1>
-    <a href="https://www.flaticon.com/kr/free-icons/" title="라면 아이콘">라면 아이콘  제작자: tulpahn - Flaticon</a>
+        <Col xs={1} md={2} lg={2}></Col>
+        <Col xs={10} md={8} lg={8}>
+        <hr></hr>
+        
+        <div className='footer'>
+          라면 추천 서비스 / &nbsp;
+        <a href="https://www.flaticon.com/kr/free-icons/" title="라면 아이콘" style={{color:'black',textDecoration:"none"}}>아이콘  제작자: tulpahn - Flaticon</a>
+
+        </div>
         </Col>
-        <Col></Col>
+        <Col xs={1} md={2} lg={2}></Col>
 
       </Row>
     </Container>
+    <style jsx>{`
+        .footer{
+          margin:10px;
+        }
+        
 
+        
+      `}</style>
     </>
 
   );
