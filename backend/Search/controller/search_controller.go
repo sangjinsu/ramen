b4f62f99f8f7b6ceba25a35f7d196e79387ae057
sangjinsu <climbplant39@kakao.com>
2022-03-28 14:13:56 +0900

    b/feat: create controller

--- backend/Search/controller/search_controller.go
+++ backend/Search/controller/search_controller.go
@@ -1,11 +1,66 @@
 package controller
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"Search/query"
+	"Search/util/elasticsearch"
+	"Search/util/loaddotenv"
+	"bytes"
+	"github.com/gofiber/fiber/v2"
+	"sync"
+)
+
+var wg sync.WaitGroup
+var elasticSearch = elasticsearch.New().Addresses(loaddotenv.LoadDotEnv("ELASTICSEARCH_SERVER")).Build()
+var searchQueries = query.SearchQueries{}
+
+type RamenResponse struct {
+	ramenId      float64
+	name         string
+	englishName  string
+	brand        string
+	englishBrand string
+	noodle       string
+	crawlingCnt  float64
+}
 
 func SearchName(c *fiber.Ctx) error {
-	return nil
+	name := c.Query("query")
+	responses := queryElasticSearch(name, searchQueries.RamenName)
+	return c.JSON(responses)
 }
 
 func SearchBrand(c *fiber.Ctx) error {
-	return nil
+	brand := c.Query("query")
+	responses := queryElasticSearch(brand, searchQueries.RamenBrand)
+	return c.JSON(responses)
+}
+
+func queryElasticSearch(name string, query func(name string) bytes.Buffer) []*RamenResponse {
+	var r map[string]interface{}
+	r = elasticSearch.Query(query(name), "ramen")
+	hits := r["hits"].(map[string]interface{})["hits"].([]interface{})
+
+	responses := []*RamenResponse{}
+
+	for _, hit := range hits {
+		source := hit.(map[string]interface{})["_source"].(map[string]interface{})
+		ramenId := source["ramen_id"].(float64)
+		ramenName := source["name"].(string)
+		englishName := source["english_name"].(string)
+		brand := source["brand"].(string)
+		englishBrand := source["english_brand"].(string)
+		noodle := source["noodle"].(string)
+		crawlingCnt := source["crawling_cnt"].(float64)
+		response := &RamenResponse{
+			ramenId:      ramenId,
+			name:         ramenName,
+			englishName:  englishName,
+			brand:        brand,
+			englishBrand: englishBrand,
+			noodle:       noodle,
+			crawlingCnt:  crawlingCnt,
+		}
+		responses = append(responses, response)
+	}
+	return responses
 }
