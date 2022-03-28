f30fab5c72d415ba0729c6df7343ad9db5cffbd8
sangjinsu <climbplant39@kakao.com>
2022-03-28 15:38:20 +0900

    b/feat: update controller

--- backend/Search/controller/search_controller.go
+++ backend/Search/controller/search_controller.go
@@ -6,21 +6,19 @@ import (
 	"Search/util/loaddotenv"
 	"bytes"
 	"github.com/gofiber/fiber/v2"
-	"sync"
 )
 
-var wg sync.WaitGroup
 var elasticSearch = elasticsearch.New().Addresses(loaddotenv.LoadDotEnv("ELASTICSEARCH_SERVER")).Build()
 var searchQueries = query.SearchQueries{}
 
 type RamenResponse struct {
-	ramenId      float64
-	name         string
-	englishName  string
-	brand        string
-	englishBrand string
-	noodle       string
-	crawlingCnt  float64
+	RamenID      float64 `json:"ramenId"`
+	Name         string  `json:"name"`
+	EnglishName  string  `json:"englishName"`
+	Brand        string  `json:"brand"`
+	EnglishBrand string  `json:"englishBrand"`
+	Noodle       string  `json:"noodle"`
+	CrawlingCnt  float64 `json:"crawlingCnt"`
 }
 
 func SearchName(c *fiber.Ctx) error {
@@ -40,27 +38,32 @@ func queryElasticSearch(name string, query func(name string) bytes.Buffer) []*Ra
 	r = elasticSearch.Query(query(name), "ramen")
 	hits := r["hits"].(map[string]interface{})["hits"].([]interface{})
 
-	responses := []*RamenResponse{}
+	ramenResponses := []*RamenResponse{}
 
 	for _, hit := range hits {
-		source := hit.(map[string]interface{})["_source"].(map[string]interface{})
-		ramenId := source["ramen_id"].(float64)
-		ramenName := source["name"].(string)
-		englishName := source["english_name"].(string)
-		brand := source["brand"].(string)
-		englishBrand := source["english_brand"].(string)
-		noodle := source["noodle"].(string)
-		crawlingCnt := source["crawling_cnt"].(float64)
-		response := &RamenResponse{
-			ramenId:      ramenId,
-			name:         ramenName,
-			englishName:  englishName,
-			brand:        brand,
-			englishBrand: englishBrand,
-			noodle:       noodle,
-			crawlingCnt:  crawlingCnt,
-		}
-		responses = append(responses, response)
+		ramenResponses = append(ramenResponses, createRamenResponse(hit))
 	}
-	return responses
+
+	return ramenResponses
+}
+
+func createRamenResponse(hit interface{}) *RamenResponse {
+	source := hit.(map[string]interface{})["_source"].(map[string]interface{})
+	ramenId := source["ramen_id"].(float64)
+	ramenName := source["name"].(string)
+	englishName := source["english_name"].(string)
+	brand := source["brand"].(string)
+	englishBrand := source["english_brand"].(string)
+	noodle := source["noodle"].(string)
+	crawlingCnt := source["crawling_cnt"].(float64)
+	response := &RamenResponse{
+		RamenID:      ramenId,
+		Name:         ramenName,
+		EnglishName:  englishName,
+		Brand:        brand,
+		EnglishBrand: englishBrand,
+		Noodle:       noodle,
+		CrawlingCnt:  crawlingCnt,
+	}
+	return response
 }
