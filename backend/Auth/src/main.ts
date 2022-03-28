6df606762468ff18a6d523ef253f9b0a7d9f6dd7
sangjinsu <climbplant39@kakao.com>
2022-03-28 09:13:38 +0900

    b/feat: add cors true

--- backend/Auth/src/main.ts
+++ backend/Auth/src/main.ts
@@ -5,7 +5,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
 
