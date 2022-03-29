826744a65646496684fa9c5b6720ba114d7706a1
K-dongil <kdi1569@gmail.com>
2022-03-29 12:45:46 +0900

    f/feat: ramen Detail Page type assing

--- front/ramen/components/Types.tsx
+++ front/ramen/components/Types.tsx
@@ -1,3 +1,38 @@
 export interface DataProps {
   data: number[];
 }
+
+export type RamenDetail = {
+  params: string;
+  ramenInfos: {
+    brand: string;
+    carbs: number;
+    cholesterol: number;
+    code: string;
+    cold: number;
+    cup: number;
+    englishBrand: string;
+    englishName: string;
+    jjajang: number;
+    kcal: number;
+    lipid: number;
+    liquid: number;
+    name: string;
+    noodle: string;
+    powder: number;
+    protein: number;
+    ramenId: number;
+    salty: number;
+    sampleId: string;
+    saturated_fat: number;
+    seasoning: number;
+    sodium: number;
+    soup: number;
+    sugar: number;
+    surveyYear: number;
+    sweetness: number;
+    transFat: number;
+    volume: number;
+  };
+  userLikeBoolean: boolean;
+};
