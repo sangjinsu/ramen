1795896f2522ff0866047afa736424f6793db93a
suhan <suhan9603@naver.com>
2022-03-29 23:08:43 +0900

    b/feat:complete log-elk

--- backend/Ramen/src/main/java/com/ramen/ramen/repository/log/LogReposiotry.java
+++
@@ -1,9 +0,0 @@
-//package com.ramen.ramen.repository.log;
-
-//import com.ramen.ramen.domain.Log;
-//import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.stereotype.Repository;
-
-//@Repository
-//public interface LogReposiotry extends ElasticsearchRepository<Log, Long> {
-//}
