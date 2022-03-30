ca52a024f2d48ed4a21e7c6b2fac72863966c780
sangjinsu <climbplant39@kakao.com>
2022-03-30 14:50:14 +0900

    b/feat: add swagger

--- backend/Recommend/server/urls.py
+++ backend/Recommend/server/urls.py
@@ -14,8 +14,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Ramen Recommend API",
+      default_version='v1',
+      description="Ramen Recommend Server",
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
-    path('v1/recommend/', include('recommend.urls'))
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('v1/recommend/', include('recommend.urls')),
 ]
