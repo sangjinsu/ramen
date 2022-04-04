27234113d9a5215f4c8d362f7d9fc19ead87e46f
Younwoo Lee <leeyw2709@gmail.com>
2022-04-05 01:37:49 +0900

    f/feat: Modify preference update page routing && Modify 짧게.jpg to 잘게.jpg

--- front/ramen/pages/user/[params]/preferenceupdate.tsx
+++ front/ramen/pages/user/[params]/preferenceupdate.tsx
@@ -1,12 +1,14 @@
 import axios from "axios";
-import { getCookie } from "cookies-next";
+import { getCookie, setCookies } from "cookies-next";
 import { withRouter, useRouter } from "next/router";
 import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import SignupPreference from "../../../components/signup/SignupPreference";
 import { userPreferenceType } from "../../../components/Types";
 import Button from "@mui/material/Button";
+import serverURLDoc from "../../../components/main/ServerURL";
 
+const AUTH_URL = serverURLDoc.AUTH_URL;
 const member_id = getCookie("member_id");
 
 const UserPreference: React.FC<userPreferenceType> = ({
@@ -131,22 +133,64 @@ const UserPreference: React.FC<userPreferenceType> = ({
   };
 
   const updatePreference = async () => {
-    try {
-      const accessToken = getCookie("accessToken");
-      await axios.put(
-        `http://j6c104.p.ssafy.io:8080/v1/member/fond?egg=${userInfo.egg}&ingredientGarlic=${userInfo.ingredientGarlic}&ingredientGreenOnion=${userInfo.ingredientGreenOnion}&ingredientNone=${userInfo.ingredientNone}&ingredientPepper=${userInfo.ingredientPepper}&memberId=${params}&noodleLength=${userInfo.noodleLength}&noodleTexture=${userInfo.noodleTexture}&spicy=${userInfo.spicy}&toppingCheese=${userInfo.toppingCheese}&toppingDumpling=${userInfo.toppingDumpling}&toppingNone=${userInfo.toppingNone}&toppingTteok=${userInfo.toppingTteok}`,
-        {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
+    let accessToken = getCookie("accessToken");
+    const refreshToken = getCookie("refreshToken");
+    axios
+      .get(`${AUTH_URL}/check-jwt`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+      .then(function (response) {
+        axios
+          .put(
+            `http://j6c104.p.ssafy.io:8080/v1/member/fond?egg=${userInfo.egg}&ingredientGarlic=${userInfo.ingredientGarlic}&ingredientGreenOnion=${userInfo.ingredientGreenOnion}&ingredientNone=${userInfo.ingredientNone}&ingredientPepper=${userInfo.ingredientPepper}&memberId=${params}&noodleLength=${userInfo.noodleLength}&noodleTexture=${userInfo.noodleTexture}&spicy=${userInfo.spicy}&toppingCheese=${userInfo.toppingCheese}&toppingDumpling=${userInfo.toppingDumpling}&toppingNone=${userInfo.toppingNone}&toppingTteok=${userInfo.toppingTteok}`,
+            {
+              headers: {
+                Authorization: `Bearer ${accessToken}`,
+              },
+            }
+          )
+          .then(function (response) {
+            router.push(`/user/${params}`);
+          });
+      })
+      .catch(function (error) {
+        console.log("check-jwt 실패", error.response.status);
+        if (error.response.status === 401) {
+          // refreshToken 유효 검사
+          axios
+            .get(`${AUTH_URL}/refresh`, {
+              headers: {
+                Authorization: `Bearer ${refreshToken}`,
+              },
+            })
+            // refreshToken 유효 - O, accessToken 갱신
+            .then(function (response) {
+              console.log("refresh 성공", response);
+              setCookies("accessToken", response.data.accessToken);
+              accessToken = response.data.accessToken;
+              axios
+                .put(
+                  `http://j6c104.p.ssafy.io:8080/v1/member/fond?egg=${userInfo.egg}&ingredientGarlic=${userInfo.ingredientGarlic}&ingredientGreenOnion=${userInfo.ingredientGreenOnion}&ingredientNone=${userInfo.ingredientNone}&ingredientPepper=${userInfo.ingredientPepper}&memberId=${params}&noodleLength=${userInfo.noodleLength}&noodleTexture=${userInfo.noodleTexture}&spicy=${userInfo.spicy}&toppingCheese=${userInfo.toppingCheese}&toppingDumpling=${userInfo.toppingDumpling}&toppingNone=${userInfo.toppingNone}&toppingTteok=${userInfo.toppingTteok}`,
+                  {
+                    headers: {
+                      Authorization: `Bearer ${accessToken}`,
+                    },
+                  }
+                )
+                .then(function (response) {
+                  router.push(`/user/${params}`);
+                });
+            })
+            .catch(function (error) {
+              alert("로그인 세션 시간이 만료되었습니다.");
+              if (error.response.status === 401) {
+                router.replace("/login");
+              }
+            });
         }
-      );
-      router.push(`/user/${params}`);
-    } catch (error) {
-      console.log(error);
-      // alert("다시 시도해주세요!!");
-      // router.push(`/user/${params}`);
-    }
+      });
   };
 
   React.useEffect(() => {
