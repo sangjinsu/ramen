e04c779f41fd93642fde47efee8029348443cc36
K-dongil <kdi1569@gmail.com>
2022-04-06 02:04:53 +0900

    f/refactor: Tab 공통 코드 component화

--- front/ramen/components/SimilarRamen.tsx
+++ front/ramen/components/SimilarRamen.tsx
@@ -1,45 +1,12 @@
 import * as React from "react";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
-import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import { DataProps, SimilarRamenType } from "./Types";
+import { TabPanel, a11yProps } from "./TabComponent";
 import BarSimilar from "./BarSimilar";
 import DocDataDictionary from "./main/dataDictionary";
 import Link from "next/link";
-
-interface TabPanelProps {
-  children?: React.ReactNode;
-  index: number;
-  value: number;
-}
-
-function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-function a11yProps(index: number) {
-  return {
-    id: `simple-tab-${index}`,
-    "aria-controls": `simple-tabpanel-${index}`,
-  };
-}
 
 export default function SimilarRamen({
   similarityRamen,
