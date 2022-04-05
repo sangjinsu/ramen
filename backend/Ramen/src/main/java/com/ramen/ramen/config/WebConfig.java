28aaebe38d402c3e0072a0de2ab6ed094304510b
suhan <suhan9603@naver.com>
2022-04-05 23:58:54 +0900

    b/feat:update cors for front

--- backend/Ramen/src/main/java/com/ramen/ramen/config/WebConfig.java
+++ backend/Ramen/src/main/java/com/ramen/ramen/config/WebConfig.java
@@ -4,14 +4,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("http://j6c104.p.ssafy.io:8888")
-//                .allowedOrigins("http://j6c104.p.ssafy.io:3000")
-//                .allowedMethods("*");
-//    }
-//}
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://j6c104.p.ssafy.io:8888")
+                .allowedOrigins("http://j6c104.p.ssafy.io:3000")
+                .allowedMethods("*");
+    }
+}
