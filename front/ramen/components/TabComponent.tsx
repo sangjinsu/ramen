572657e7895792a49c17f43bc15a61486f306e37
K-dongil <kdi1569@gmail.com>
2022-04-06 13:56:57 +0900

    f/fix: ramenList page Tab fix, chartjs props data rollback

--- front/ramen/components/TabComponent.tsx
+++ front/ramen/components/TabComponent.tsx
@@ -23,6 +23,23 @@ export function TabPanel(props: TabPanelProps) {
   );
 }
 
+export function TabPanel1(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      <Box sx={{ p: 3 }}>
+        <Typography>{children}</Typography>
+      </Box>
+    </div>
+  );
+}
+
 export function a11yProps(index: number) {
   return {
     id: `simple-tab-${index}`,
