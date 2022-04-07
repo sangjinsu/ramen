8588cdbc64ac45f961fe62f28ff3f19bce4204fe
김지원 <flykimjiwun@naver.com>
2022-04-07 19:35:01 +0900

    f/feat:test

--- front/ramen/components/main/Suggestion.js
+++ front/ramen/components/main/Suggestion.js
@@ -7,6 +7,7 @@ import Link from "next/link";
 
 export default function Suggestion(props) {
   let [ramen, setRamen] = useState([]);
+  let [ai,setAi] = useState(false)
   // let [sugSwitch,setSugSwitch] = useState(false)
   let titleString = {
     "IBCF추천": "내가 좋아하는 라면과 비슷한",
@@ -38,9 +39,11 @@ export default function Suggestion(props) {
         .get(`http://j6c104.p.ssafy.io:8888/v1/recommend/dbrc/${props.id}`)
         .then((result) => {
           setRamen(result.data);
+          setAi(true)
           // 이걸로테스트가능 3항연산자
           // setSugSwitch(true)
         })
+        
     }
   }, []);
   useEffect(() => {
@@ -131,7 +134,7 @@ export default function Suggestion(props) {
             </ListGroup.Item>
           </>
         ) :
-          (ramen.length ===0
+          (ai ===false
             ?<><ListGroup.Item>AI가 학습중입니다.</ListGroup.Item></>
             :<><ListGroup.Item>{textString[props.title]}</ListGroup.Item></>
 
