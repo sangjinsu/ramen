9d6af1b1f851416882d2ed6af8e9c89e0c7af5a4
sangjinsu <climbplant39@kakao.com>
2022-04-07 14:50:02 +0900

    b/faet: update cors

--- backend/Auth/src/main.ts
+++ backend/Auth/src/main.ts
@@ -5,7 +5,11 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: [process.env.EC2_SERVER, process.env.GATEWAY_SERVER],
+    },
+  });
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
 
